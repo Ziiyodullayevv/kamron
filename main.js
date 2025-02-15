47e55f33d5ce107f8a368e9d2d1507c4a8aac73b
Ziyodullayev Akobir <124231669+Ziiyodullayevv@users.noreply.github.com>
2025-02-15 22:49:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let umarText = document.querySelector(".umar");
+console.log(umarText);
+
+function umar() {
+  umarText.textContent = "Ruxsat!";
+  umarText.style.color = "red";
+  umarText.style.backgroundColor = "";
+}
+
+
+
+function umar2() {
+  umarText.textContent = "Xush Kelibsiz💙";
+  umarText.style.color = "";
+  umarText.style.backgroundColor = "";
+}
